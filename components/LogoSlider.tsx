c2ee61c32f8434ebf267ee5a26ea113c7334b4c3
Gagandeep Singh <gagansday@gmail.com>
2025-02-28 17:50:20 +0530

    add all client logos in logo slider

--- components/LogoSlider.tsx
+++ components/LogoSlider.tsx
@@ -1,65 +1,97 @@
+import aladdinmedia from "@/assets/client-logos/aladdinmedia.png";
+import asnasoftwaresolution from "@/assets/client-logos/asnasoftwaresolution.png";
+import attarshastra from "@/assets/client-logos/attarshastra.png";
+import bhagyashaaliWelfareFoundation from "@/assets/client-logos/bhagyashaali-welfare-foundation.png";
+import chromaTechServices from "@/assets/client-logos/chroma-tech-services.webp";
+import cityLineCollection from "@/assets/client-logos/city-line-collection.png";
+import companiesHouse from "@/assets/client-logos/companies-house.svg";
+import dietitianMiksha from "@/assets/client-logos/dietitian-miksha.png";
+import emerhub from "@/assets/client-logos/emerhub.svg";
+import gaadiBuy from "@/assets/client-logos/gaadi-buy.png";
+import galeciaGroup from "@/assets/client-logos/galecia-group.png";
+import globusMachinesPvtLtd from "@/assets/client-logos/globus-machines-pvt-ltd.png";
+import hasten from "@/assets/client-logos/hasten.png";
+import heavenlyHome from "@/assets/client-logos/heavenly-home.png";
+import jyotiAyurClinicLogo from "@/assets/client-logos/jyoti-ayur-clinic-logo.png";
+import lazyBaby from "@/assets/client-logos/lazy-baby.svg";
+import mspGlobal from "@/assets/client-logos/msp-global.png";
+import myDreamHomeCare from "@/assets/client-logos/my-dream-home-care.png";
+import niyaOverseas from "@/assets/client-logos/niya-overseas.svg";
+import onKampus from "@/assets/client-logos/onkampus.svg";
+import organishHerbal from "@/assets/client-logos/organish-herbal.png";
+import seoscout from "@/assets/client-logos/seoscout.svg";
+import texTrend from "@/assets/client-logos/tex-trend.svg";
+import theInsuranceProblem from "@/assets/client-logos/the-insurance-problem.png";
+import theNutritionBox from "@/assets/client-logos/the-nutrition-box.png";
+import theAirApp from "@/assets/client-logos/theairapp.svg";
+import triStates from "@/assets/client-logos/tri-states.png";
+import upkarGroup from "@/assets/client-logos/upkar-group.png";
+import vTeach from "@/assets/client-logos/v-teach.png";
+import vkCarLoan from "@/assets/client-logos/vk-car-loan.png";
+import vska from "@/assets/client-logos/vska.png";
+import vyzionInnovations from "@/assets/client-logos/vyzion-innovations.svg";
+import watsupUsa from "@/assets/client-logos/watsup-usa.png";
+
+import { cn } from "@/lib/utils";
+import Image from "next/image";
 interface Logo {
-  id: string;
-  description: string;
   image: string;
   className?: string;
 }
-
-const logos = [
+// invert grayscale brightness-0
+const clientLogos: Logo[] = [
+  { image: aladdinmedia, className: "h-8 brightness-0 invert" },
   {
-    id: "logo-1",
-    description: "Logo 1",
-    image: "https://shadcnblocks.com/images/block/logos/astro.svg",
-    className: "h-7 w-auto",
+    image: asnasoftwaresolution,
+    className: "h-10 brightness-0 invert",
   },
+  { image: attarshastra, className: "h-10 invert brightness-0" },
   {
-    id: "logo-2",
-    description: "Logo 2",
-    image: "https://shadcnblocks.com/images/block/logos/figma.svg",
-    className: "h-7 w-auto",
+    image: bhagyashaaliWelfareFoundation,
+    className: "h-10 invert brightness-0",
   },
-  {
-    id: "logo-3",
-    description: "Logo 3",
-    image: "https://shadcnblocks.com/images/block/logos/nextjs.svg",
-    className: "h-7 w-auto",
-  },
-  {
-    id: "logo-4",
-    description: "Logo 4",
-    image: "https://shadcnblocks.com/images/block/logos/react.png",
-    className: "h-7 w-auto",
-  },
-  {
-    id: "logo-5",
-    description: "Logo 5",
-    image: "https://shadcnblocks.com/images/block/logos/shadcn-ui.svg",
-    className: "h-7 w-auto",
-  },
-  {
-    id: "logo-6",
-    description: "Logo 6",
-    image: "https://shadcnblocks.com/images/block/logos/supabase.svg",
-    className: "h-7 w-auto",
-  },
-  {
-    id: "logo-7",
-    description: "Logo 7",
-    image: "https://shadcnblocks.com/images/block/logos/tailwind.svg",
-    className: "h-4 w-auto",
-  },
-  {
-    id: "logo-8",
-    description: "Logo 8",
-    image: "https://shadcnblocks.com/images/block/logos/vercel.svg",
-    className: "h-7 w-auto",
-  },
+  { image: chromaTechServices, className: "h-10 invert brightness-0" },
+  { image: cityLineCollection, className: "h-9 invert brightness-0" },
+  { image: companiesHouse, className: "h-10 invert" },
+  { image: dietitianMiksha, className: "h-10 invert brightness-0" },
+  { image: emerhub, className: "h-10 invert" },
+  { image: gaadiBuy, className: "h-10 invert brightness-0" },
+  { image: galeciaGroup, className: "h-16 invert brightness-0" },
+  { image: globusMachinesPvtLtd, className: "h-10 invert brightness-0" },
+  { image: hasten, className: "h-12 invert brightness-0" },
+  { image: heavenlyHome, className: "h-14 invert brightness-0" },
+  { image: jyotiAyurClinicLogo, className: "h-10 invert brightness-0" },
+  { image: lazyBaby, className: "h-10 invert brightness-0" },
+  { image: onKampus, className: "h-10 invert brightness-0" },
+  { image: mspGlobal, className: "h-10 invert brightness-0" },
+  { image: myDreamHomeCare, className: "h-12 invert brightness-0" },
+  { image: niyaOverseas, className: "h-10 invert brightness-0" },
+  { image: organishHerbal, className: "h-10 invert brightness-0" },
+  { image: seoscout, className: "h-10 invert grayscale brightness-50" },
+  { image: texTrend, className: "h-10 invert brightness-0" },
+  { image: theInsuranceProblem, className: "h-10 invert brightness-0" },
+  { image: theNutritionBox, className: "h-10" },
+  { image: theAirApp, className: "h-10 invert grayscale brightness-50" },
+  { image: triStates, className: "h-10 invert brightness-0" },
+  { image: upkarGroup, className: "h-10 invert brightness-0" },
+  { image: vTeach, className: "h-10 invert brightness-0" },
+  { image: vkCarLoan, className: "h-10 invert brightness-0" },
+  { image: vska, className: "h-10 invert brightness-0" },
+  { image: vyzionInnovations, className: "h-10 invert brightness-0" },
+  { image: watsupUsa, className: "h-10 invert brightness-0" },
 ];
 
 export default function LogoSlider() {
   return (
-    <div className="bg-white/20 flex items-center justify-center h-12">
-      Logos
+    <div className="flex items-center gap-14 overflow-scroll">
+      {clientLogos.map((logo, index) => (
+        <Image
+          key={index}
+          src={logo.image}
+          alt=""
+          className={cn(`${logo.className}`, "w-fit flex-none")}
+        />
+      ))}
     </div>
   );
 }
